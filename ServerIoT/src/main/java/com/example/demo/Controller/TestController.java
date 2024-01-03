ee2da509a9b35b642d295ddc20e96fde1f7bb7d2
thanhlam0241 <80621994+thanhlam0241@users.noreply.github.com>
2024-01-03 13:30:51 +0700

    fix bugs

--- ServerIoT/src/main/java/com/example/demo/Controller/TestController.java
+++ ServerIoT/src/main/java/com/example/demo/Controller/TestController.java
@@ -1,23 +1,34 @@
 package com.example.demo.Controller;
 
 
+import com.example.demo.Entites.User;
+import com.example.demo.Repository.UserRepository;
 import com.example.demo.Services.AttendanceLogService;
 import com.example.demo.Services.AttendanceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/test")
 @RequiredArgsConstructor
 public class TestController {
-    private final AttendanceLogService service;
-
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     @GetMapping
     public ResponseEntity<String> updateLogs(){
-        service.testUpdate();
+        List<User> users = userRepository.findAll();
+
+        for(User user : users){
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            userRepository.save(user);
+        }
+
         return ResponseEntity.ok("Hello");
     }
 }
