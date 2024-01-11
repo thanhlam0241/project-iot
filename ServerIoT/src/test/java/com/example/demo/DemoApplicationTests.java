6f5383bfb8a546ed8d2364ca02d576a080e5b803
thanhlam0241 <80621994+thanhlam0241@users.noreply.github.com>
2024-01-11 14:08:10 +0700

    update

--- ServerIoT/src/test/java/com/example/demo/DemoApplicationTests.java
+++ ServerIoT/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -9,13 +9,4 @@ import java.util.List;
 
 @SpringBootTest
 class DemoApplicationTests {
-	@Autowired
-	private ManagementUnitRepository managementUnitRepository;
-	@Test
-	void contextLoads() {
-		var managementUnits = managementUnitRepository.findAll();
-		List<String> listId = managementUnits.stream().map(managementUnit -> managementUnit.getId()).toList();
-		System.out.println(String.join(",", listId));
-	}
-
 }
